2aba776b96ef1079000f9cdf3824108806a2d04d
joseronierison <ronierison.silva@gmail.com>
2019-04-21 13:18:59 -0300

    Remove mongodb password of testing env

--- src/main/scala/infrastructure/config/MongoDBConfig.scala
+++ src/main/scala/infrastructure/config/MongoDBConfig.scala
@@ -5,17 +5,9 @@ import com.google.inject.Singleton
 @Singleton
 class MongoDBConfig extends Configuration {
   private val ROOT = "mongodb"
-  private val HOST = s"$ROOT.host"
-  private val PORT = s"$ROOT.port"
   private val DATABASE = s"$ROOT.database"
-  private val USERNAME = s"$ROOT.username"
-  private val PASSWORD = s"$ROOT.password"
   private val CONNECTION_STRING = s"$ROOT.connection_string"
 
-  def host: String = config.getString(HOST)
-  def port: Int = config.getInt(PORT)
   def database: String = config.getString(DATABASE)
-  def username: String = config.getString(USERNAME)
-  def password: String = config.getString(PASSWORD)
   def connectionString: String = config.getString(CONNECTION_STRING)
 }
--- src/test/scala/unit/infrastructure/config/MongoDBConfigTest.scala
+++ src/test/scala/unit/infrastructure/config/MongoDBConfigTest.scala
@@ -5,29 +5,14 @@ import support.VisionSpec
 
 class MongoDBConfigTest extends VisionSpec {
   private val config = new MongoDBConfig()
+  val databaseName = "company_configs_test"
 
   behavior of "MongoDBConfig"
   it should "return connectionString" in {
-    config.connectionString shouldEqual """mongodb://mongodb:mongodb@localhost:27017/admin"""
+    config.connectionString shouldEqual s"""mongodb://localhost/$databaseName"""
   }
 
   it should "return database" in {
-    config.database shouldEqual "company_configs_test"
-  }
-
-  it should "return password" in {
-    config.password shouldEqual "mongodb"
-  }
-
-  it should "return username" in {
-    config.username shouldEqual "mongodb"
-  }
-
-  it should "return host" in {
-    config.host shouldEqual "localhost"
-  }
-
-  it should "return port" in {
-    config.port shouldEqual 27017
+    config.database shouldEqual databaseName
   }
 }
